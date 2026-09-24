5e382a5beed63d44825a2a6957878a7078e15624
Edinah00 <edinah24.aphrodite@gmail.com>
2026-09-25 00:59:34 +0300

    Sprint6

---
+++ src/main/java/mg/itu/FrameworkEwin/annotations/methods/RestAPI.java
@@ -0,0 +1,9 @@
+package mg.itu.FrameworkEwin.annotations.methods;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RestAPI {
+
+}
--- src/main/java/mg/itu/FrameworkEwin/controllers/FrontControllerServlet.java
+++ src/main/java/mg/itu/FrameworkEwin/controllers/FrontControllerServlet.java
@@ -17,6 +17,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import mg.itu.FrameworkEwin.annotations.utils.Utils;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import mg.itu.FrameworkEwin.models.*;
 import mg.itu.FrameworkEwin.annotations.methods.*;
 
@@ -42,18 +43,14 @@ public class FrontControllerServlet extends HttpServlet {
         String contexte = request.getContextPath();
         String urlRelative = urlComplete.substring(contexte.length());
 
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>Tonga eto tsika aaaaa le Framework !</h1>");
-        out.println("<p>URL complete : <b>" + urlComplete + "</b></p>");
-        out.println("<p>URL relative <b>" + urlRelative + "</b></p>");
+        // Ne pas écrire d'HTML global ici : on renvoie soit JSON (REST), soit on forward vers JSP
         Map<URLMethod, Mapping> routes = (Map<URLMethod, Mapping>) getServletContext().getAttribute("MapURL");
         String methodHttp = request.getMethod();
 
         URLMethod key = new URLMethod(urlRelative, methodHttp);
 
         Mapping mapping = routes.get(key);
-
+        PrintWriter out = response.getWriter();
         if (mapping != null) {
             try {
                 Class<?> clazz = Class.forName(mapping.getClassName());
@@ -67,11 +64,46 @@ public class FrontControllerServlet extends HttpServlet {
                     String viewName = modelAndView.getViewName();
                     Map<String, Object> modelData = modelAndView.getDonnéeÀafficher();
 
+                    
                     for (Map.Entry<String, Object> entry : modelData.entrySet()) {
                         request.setAttribute(entry.getKey(), entry.getValue());
                     }
-                    System.out.println("Attributs envoyés à la vue : " + modelData);
-                    request.getRequestDispatcher("/WEB-INF/views/" + viewName + ".jsp").forward(request, response);
+                    if (method.isAnnotationPresent(RestAPI.class)) {
+                        response.setContentType("application/json");
+                        response.setCharacterEncoding("UTF-8");
+
+                        ObjectMapper mapper = new ObjectMapper();
+                        // Pour une méthode qui renvoie ModelAndView et annotée RestAPI,
+                        // on sérialise les données du modèle (map) plutôt que l'objet ModelAndView
+                        String json = mapper.writeValueAsString(modelData);
+
+                        response.getWriter().write(json);
+                        return;
+                    } else {
+                        System.out.println("Attributs envoyés à la vue : " + modelData);
+                        request.getRequestDispatcher("/WEB-INF/views/" + viewName + ".jsp").forward(request, response);
+                        return;
+                    }
+                    
+                }
+                // Si le résultat n'est pas un ModelAndView
+                if (method.isAnnotationPresent(RestAPI.class)) {
+                    response.setContentType("application/json");
+                    response.setCharacterEncoding("UTF-8");
+
+                    ObjectMapper mapper = new ObjectMapper();
+                    String json = mapper.writeValueAsString(result);
+
+                    response.getWriter().write(json);
+                    return;
+                } else {
+                    // Pas de JSP ni REST : on affiche simplement la représentation texte
+                    out.println("<html><body>");
+                    out.println("<h1>Tonga eto tsika aaaaa le Framework !</h1>");
+                    out.println("<p>URL complete : <b>" + urlComplete + "</b></p>");
+                    out.println("<p>URL relative <b>" + urlRelative + "</b></p>");
+                    out.println("<pre>" + String.valueOf(result) + "</pre>");
+                    out.println("</body></html>");
                     return;
                 }
 
@@ -79,7 +111,6 @@ public class FrontControllerServlet extends HttpServlet {
                 throw new ServletException(e);
             }
         } else {
-
             out.println("<html><body>");
             out.println("<h2>Liste des routes disponibles</h2>");
             for (Map.Entry<URLMethod, Mapping> entry : routes.entrySet()) {
